388fb92e15ef397fbb51eead36f6ae90350c9991
Spongiforma <ooh.nooess@gmail.com>
2022-04-10 16:55:04 +0800

    remove build

--- build/CMakeFiles/MU.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/MU"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MU.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
